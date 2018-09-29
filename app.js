eea5a11cfcfa6d10edd06b14231d30f5796f2789
Sideshow Mel <sideshowmel@Erics-MacBook-Pro.local>
2018-09-28 20:52:23 -0700

    Users can edit and update reviews

--- app.js
+++ app.js
@@ -22,16 +22,24 @@ app.get('/reviews/new', (req, res) => {
   res.render('reviews-new', {});
 })
 
+
 // CREATE
 app.post('/reviews', (req, res) => {
   Review.create(req.body).then((review) => {
-    console.log(review);
-    res.redirect('/');
+    console.log(review)
+    res.redirect(`/reviews/${review._id}`) // Redirect to reviews/:id
+  }).catch((err) => {
+    console.log(err.message)
+  })
+})
+// SHOW
+app.get('/reviews/:id', (req, res) => {
+  Review.findById(req.params.id).then((review) => {
+    res.render('reviews-show', { review: review })
   }).catch((err) => {
     console.log(err.message);
   })
 })
-
 // OUR MOCK ARRAY OF PROJECTS
 //let reviews = [
 //  { title: "Great Review" },
