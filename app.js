6c65e0e131109f8af71087a8996c8cac7d96e55e
Sideshow Mel <sideshowmel@Erics-MacBook-Pro.local>
2018-09-28 19:34:07 -0700

    Added MongoDB and Review model

--- app.js
+++ app.js
@@ -1,17 +1,32 @@
 const express = require('express')
 const app = express()
 
-
-
+// INDEX
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
 
-app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
-})
 
+// OUR MOCK ARRAY OF PROJECTS
+//let reviews = [
+//  { title: "Great Review" },
+  //{ title: "Next Review" },
+//  {title:  "Complete Trash"}
+//]
 
+//stuff for mongoose
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotp', { useMongoClient: true });
+
+const Review = mongoose.model('Review', {
+  title: String
+});
 // app.js
 var exphbs = require('express-handlebars');
 
